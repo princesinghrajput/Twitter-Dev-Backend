6a1a23a07704898b599543e3d84ad0a79cdfe5c4
Prince5632 <prince.kumar@sensationsolutions.in>
2024-01-18 17:36:31 +0530

    Till how to connect to mogodb

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const connect = require('./config/database');
+const app = express();
+
+app.listen(3000, async() =>{
+    console.log('Server is running on port 3000');
+    await connect();
+    console.log("Mongo db connected")
+
+});
